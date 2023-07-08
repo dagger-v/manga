781bed48c78074f1c14c46062a2abe61a2c21db5
dagger-v <goingarcher@gmail.com>
2023-07-08 16:57:30 -0700

    added profile page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/profile", function (req, res, next) {
+  res.render("profile", { title: "Taka" });
 });
 
 module.exports = router;
